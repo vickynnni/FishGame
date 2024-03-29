542b7df6f5cf0feec8a4609a5c4071b0d2f9849c
viiiccv <95257527+viiiccv@users.noreply.github.com>
2024-03-29 20:18:40 +0100

    boids

--- scripts/fish.gd
+++ scripts/fish.gd
@@ -1,13 +1,30 @@
 class_name Fish
 extends Area2D
 
+var velocity = Vector2(5.0,0.0)
+var drag = 1
+var close_d = Vector2(0.0,0.0)
+var separation = 20
+var avoidfactor = 0.005
 
-var velocity = Vector2(0.0,0.0)
+var neighboring_boids = 0
+var vel_avg = Vector2(0.0,0.0)
+var pos_avg = Vector2(0.0,0.0)
+var visible_range = 80
+var matching_factor = 0.1
+var centering_factor = 0.0005
+
+# max_velocity -> check
+# bborders
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	position = Vector2(300.0,300.0)
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	position += velocity
+	velocity *= drag
+	rotation = velocity.angle()
+	position += velocity 
 	pass
--- scripts/main_scene.gd
+++ scripts/main_scene.gd
@@ -1,10 +1,102 @@
 extends Node
 
+var FishClass = preload("res://regular_fish.tscn")
+var num_fish = 10
+var fish_list = []
+#var leftmargin = 2000
+#var rightmargin = 2000
+#var topmargin = 4000
+#var bottommargin = 3000	
+var turnfactor = 0.2
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	for n in num_fish:
+		var f = FishClass.instantiate()
+		add_child(f)
+		f.position += Vector2(randf_range(-10.0,10.0),randf_range(-10.0,10.0))
+
+		fish_list.append(f)
 	pass # Replace with function body.
+
+func separation():
+	for fish in fish_list:
+		fish.close_d = Vector2(0.0,0.0)
+		var distance = Vector2(0.0,0.0)
+		var threshold = fish.separation
+		for other_fish in fish_list:
+			if fish == other_fish:
+				break
+			distance = fish.position - other_fish.position
+			if (distance.length() <= threshold):
+				fish.close_d += distance
+		fish.velocity += fish.close_d * fish.avoidfactor
+		detect_wall_collisions(fish)
+
+
+func alignment():
+	for fish in fish_list:
+		fish.neighboring_boids = 0
+		fish.vel_avg = Vector2(0.0,0.0)
+		var distance = Vector2(0.0,0.0)
+		for other_fish in fish_list:
+			distance = fish.position - other_fish.position
+			if (distance.length() < fish.visible_range):
+				fish.vel_avg += other_fish.vel_avg
+				fish.neighboring_boids += 1
+		if (fish.neighboring_boids > 0):
+			fish.vel_avg = fish.vel_avg/fish.neighboring_boids
+		fish.velocity += (fish.vel_avg)* fish.matching_factor
+		detect_wall_collisions(fish)
+
+
+func cohesion():
+	for fish in fish_list:
+		fish.pos_avg = Vector2(0.0,0.0)
+		fish.neighboring_boids = 0
+		for other_fish in fish_list:
+			fish.pos_avg += other_fish.position
+			fish.neighboring_boids += 1
+		if (fish.neighboring_boids > 0):
+			fish.pos_avg = fish.pos_avg/fish.neighboring_boids
+		fish.velocity += (fish.pos_avg - fish.position)*fish.centering_factor
+		detect_wall_collisions(fish)
+
+func detect_wall_collisions(fish):
+	var rightmargin = 1280
+	var bottommargin = 720
+	var topmargin = 0
+	var leftmargin = 0
+	var dist_right_border = rightmargin - fish.position.x
+	var dist_left_border = leftmargin - fish.position.x
+	var dist_top_border =  fish.position.y - topmargin
+	var dist_bottom_border = bottommargin - fish.position.y
+	
+	if (fish.position.x >= rightmargin):
+		fish.velocity.x = -fish.velocity.x
+	else:
+		fish.velocity.x = fish.velocity.x + (-1/dist_right_border)
+	
+	if (fish.position.x <= 0):
+		fish.velocity.x = -fish.velocity.x
+	else:
+		fish.velocity.x = fish.velocity.x + (1/dist_left_border)
+	
+	if (fish.position.y <= 0):
+		fish.velocity.y = -fish.velocity.y
+	else:
+		fish.velocity.y = fish.velocity.y + (1/dist_top_border)
+	
+	if (fish.position.y >= bottommargin):
+		fish.velocity.y = -fish.velocity.y
+	else:
+		fish.velocity.y = fish.velocity.y + (-1/dist_bottom_border)
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	print(get_viewport().get_visible_rect().size)
+	separation()
+	alignment()
+	cohesion()
 	pass
